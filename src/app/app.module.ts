eb5a876e91531131e373227bd8aa87a38ec7b201
aheduardo5 <aheduardo5@gmail.com>
2023-05-17 11:39:47 -0600

    register form has been modified. Reactive form has beed added to the component to work on it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -30,7 +32,8 @@ import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-}
+
+    formGroupRegister!: FormGroup;
+
+      constructor( private fb: FormBuilder){}
+
+      ngOnInit(): void {
+        this.formGroupRegister = this.fb.group({
+          name: ['',[ Validators.required, Validators.minLength(5)]],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(10)]]
+        })
+        
+      }
+
+      createUser(){
+        console.log(this.formGroupRegister.value);
+      }
+
+  }
